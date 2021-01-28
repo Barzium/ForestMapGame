bf95358a16dc37bf598a7a44109526d4406b0355
Rei <reisegev@yahoo.com>
2021-01-28 22:05:20 +0200

    Player Controller

---
+++ Assets/Mouse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed, rotationUpAndDownLimit;
+    float mouseY, mouseX, rotationX;
+
+    [SerializeField] Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotation();
+    }
+    void Rotation()
+    {
+        mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+        mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -rotationUpAndDownLimit, rotationUpAndDownLimit);
+
+
+
+       transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController playerController;
+    Vector3 inputVector;
+    [SerializeField] float movementSpeed;
+    float gravity = -9.81f;
+
+    Vector3 gravityVector;
+    private void Update()
+    {
+        GetInput();
+      
+    }
+    public void GetInput()
+    {
+            inputVector = transform.right * Input.GetAxis("Horizontal") +
+            transform.forward * Input.GetAxis("Vertical");
+
+        inputVector *= movementSpeed*Time.deltaTime;
+        
+        if (playerController != null)
+        {
+            playerController.Move(inputVector);
+            gravityVector.y += gravity * Time.deltaTime;
+            playerController.Move(gravityVector * Time.deltaTime);
+        }
+    }
+
+
+
+}

876e11a1f34c4cdc7b33d872e52465ec37a6c1d5
Rei <reisegev@yahoo.com>
2021-01-30 01:08:35 +0200

    added particles and playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] CharacterController playerController;
     Vector3 inputVector;
     [SerializeField] float movementSpeed;
+    [SerializeField] float speed;
     float gravity = -9.81f;
 
     public Transform groundCheck;
@@ -33,7 +34,12 @@ public class PlayerController : MonoBehaviour
         
         if (playerController != null)
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+                playerController.Move(inputVector * speed);
+            else
             playerController.Move(inputVector);
+
+
             gravityVector.y += gravity * Time.deltaTime;
             playerController.Move(gravityVector * Time.deltaTime);
         }
--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,49 @@ public class TimeManager : MonoBehaviour
 {
     [SerializeField]Transform candle;
     [SerializeField]Animator anim;
-    Vector3 finalPosition;
-   [SerializeField] float currentTime, MaxTime;
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] Light lightTorch;
+   [SerializeField] float MaxTime , StartTimeBeforeConsumption;
+    float lowestPoint = 0.16f;
+    Vector3 startPos = new Vector3();
     private void Start()
+    {  
+        startPos = candle.position;
+        StopCoroutine(Melting());
+        StartCoroutine(Melting());
+
+    }
+    float endY;
+    IEnumerator Melting()
     {
-        finalPosition = candle.position;
+        yield return new WaitForSeconds(StartTimeBeforeConsumption);
+
+      
+         endY =candle.position.y - lowestPoint;
+
+        LeanTween.moveY(candle.gameObject, endY, MaxTime);
+        
+        yield return new WaitForSeconds(MaxTime);
+        Death();
     }
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        RestartSetting();
     }
+    void Death() {
+        anim.enabled = false;
+        sr.enabled = false;
+        lightTorch.enabled = false;
+    }
+    public void RestartSetting() {
+        LeanTween.reset();
+        LeanTween.moveY(candle.gameObject, candle.position.y + lowestPoint, 1f);
+        anim.enabled = true;
+        sr.enabled = true;
+        lightTorch.enabled = true;
+        StopAllCoroutines();
+        StartCoroutine(Melting());
+    }
+ 
 }
